848cfbd8175afa8a6cbd5d444daf63deab29b70a
henryhschen <tool830@gmail.com>
2018-10-30 22:39:26 +0800

    update

--- Markops.py
+++ Markops.py
@@ -1,10 +1,8 @@
 #!/usr/local/bin/python3.6
 
-import re, json, collections, argparse
+import re, json, collections, argparse, matplotlib
 import pandas as pd
 import numpy as np
-import matplotlib
-
 
 parser = argparse.ArgumentParser(description='Process for Markops')
 parser.add_argument('config',nargs='?', help='Input config file', default="naming")
@@ -12,14 +10,6 @@ args = parser.parse_args()
 print("-config", args.config)
 
 test = collections.defaultdict(lambda: collections.defaultdict(dict))
-
-test["PV"]["name"] = "Production level"
-test["PV"]["unit"] = "KU"
-test["PC"]["name"] = "Production capacity"
-test["PC"]["unit"] = "KU"
-
-#with open("data", "w") as f:
-#    json.dump(test, f, indent=4)
 
 with open("data", "rt") as f:
     config = json.load(f)
